c6ba5d0b848316dc05e74a82f1681b53fb8093c2
Thiago Eduardo <playsd395@gmail.com>
2023-07-12 20:18:58 -0300

    Criado app cliente

---
+++ cliente/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from datetime import datetime
+
+class DadosPessoais(models.Model):
+    nome_completo = models.CharField(max_length=60)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11)
+    rg = models.CharField(max_length=10)
+    email = models.CharField(max_length=60)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome_completo
+
+class Endereco(models.Model):
+    logradouro = models.CharField(max_length=120)
+    numero = models.IntegerField()
+    bairro = models.CharField(max_length=30)
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.logradouro
+
+
+class Conta(models.Model):
+    numero_conta = models.CharField(max_length=12)
+    agencia = models.CharField(max_length=6)
+    saldo = models.FloatField()
+    data_abertura = models.DateField(default=datetime.now())
+    status_conta = models.CharField(max_length=12)
+    
---
+++ cliente/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Dados_Pessoais, Endereco, Conta
+
+def home(request):
+    return HttpResponse('Olá mundo')
+
+def cadastrar_conta(request):
+    if request.method == 'GET':
+        ...
+    
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        cpf = request.POST.get('cpf')
+        telefone = request.POST.get('telefone')
+        rg = request.POST.get('rg')
+        email = request.POST.get('email')
+        data_nascimento = request.POST.get('data_nascimento')
+
+        logradouro = request.POST.get('logradouro')
+        numero = request.POST.get('numero')
+        bairro = request.POST.get('bairro')
+        cidade = request.POST.get('cidade')
+        estado = request.POST.get('estado')
+
+        return redirect('home')
+
+
+
+
