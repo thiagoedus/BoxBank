700fa35daa4e72af73b3582d6c0693ea6eb38f46
Thiago Santos <playsd395@gmail.com>
2024-09-08 20:03:07 -0300

    new: API para dados de pagamento de boleto

--- core/api.py
+++ core/api.py
@@ -1,6 +1,8 @@
 from ninja import NinjaAPI
 from cliente.api import cliente_router
+from operacoes.api import operacoes_router
 
 api = NinjaAPI()
 
 api.add_router('clientes/', cliente_router)
+api.add_router('operacoes/', operacoes_router)
---
+++ operacoes/api.py
@@ -0,0 +1,23 @@
+from ninja import Router
+from .models import Boleto
+from cliente.models import Conta
+from django.shortcuts import get_object_or_404
+
+operacoes_router = Router()
+
+@operacoes_router.get('/{codigo_boleto}')
+def get_cliente_for_pix(request, codigo_boleto: str):
+    boleto = get_object_or_404(Boleto, codigo=codigo_boleto)
+    conta_beneficiaria = Conta.objects.get(pk=boleto.conta_beneficiaria.id)
+    response = {"valor": boleto.valor, "emissor": conta_beneficiaria.banco, "favorecido":\
+                 conta_beneficiaria.cliente.nome_completo}
+    return response
+
+# Valor
+# Pagador
+# Agencia
+# Conta
+
+# Favorecido
+# Emissor
+# Vencimento
