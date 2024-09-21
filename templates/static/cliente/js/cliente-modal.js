d2ac583a54bba409ace8c28ddbca06b00eddba8b
Thiago Santos <playsd395@gmail.com>
2024-09-20 23:50:46 -0300

    new: Modal com next step para pagamento de PIX

--- templates/static/cliente/js/cliente-modal.js
+++ templates/static/cliente/js/cliente-modal.js
@@ -1,9 +1,9 @@
+// Aqui o modal será aberto ou fechado
 const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
 const fade = document.querySelector("#fade");
 
-//TODO Escolher qual modal virá para esta função
 const toggleModal = () => {
   modal.classList.toggle("hide");
   fade.classList.toggle("hide");
@@ -12,3 +12,33 @@ const toggleModal = () => {
 [openModalButton, closeModalButton, fade].forEach((el) => {
   el.addEventListener("click", () => toggleModal());
 });
+
+// Responsável por fazer o next step do modal
+const modalSteps = document.querySelectorAll('.modal-all')
+const prevBtns = document.querySelectorAll('.button-prev');
+const nextBtns = document.querySelectorAll('.button-next');
+
+let modalStepsNum = 0;
+
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modalStepsNum++;
+    updateModalSteps();
+  });
+});
+
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modalStepsNum--;
+    updateModalSteps();
+  });
+});
+
+function updateModalSteps() {
+  modalSteps.forEach(modalStep => {
+    modalStep.classList.contains('modal-step-active') && modalStep.classList.remove('modal-step-active')
+  })
+
+  modalSteps[modalStepsNum].classList.add("modal-step-active")
+
+}
