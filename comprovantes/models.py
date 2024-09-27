3810ade71428e963ec08c3908c5617f15b9a0c1e
Thiago Santos <playsd395@gmail.com>
2024-09-27 16:17:34 -0300

    new: new model comprovante pix

--- comprovantes/models.py
+++ comprovantes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from cliente.models import Conta
+from decimal import Decimal
 
-# Create your models here.
+class ComprovantePIX(models.Model):
+    conta_pagador = models.ForeignKey(Conta, related_name="conta_pagador", on_delete=models.DO_NOTHING, editable=False)
+    conta_beneficiario = models.ForeignKey(Conta, related_name="conta_beneficiario", on_delete=models.DO_NOTHING, editable=False)
+    data_hora_transacao = models.DateTimeField(auto_now_add=True, editable=False)
+    banco_pagador = models.CharField(max_length=10, default="0934", editable=False)
+    banco_beneficiario = models.CharField(max_length=10, default="0", editable=False)
+    valor = amount = models.DecimalField(max_digits=15, decimal_places=2, default=Decimal(0.00), editable=False, editable=False)   
+    chave_pix = models.CharField(max_length=255, editable=False)
